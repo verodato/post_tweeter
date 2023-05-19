12ab16010699408d552749dc41c4b31cd8abb2a1
sander <lacerda.sander@gmail.com>
2023-05-19 11:27:57 -0300

    thread

--- easy_post_twitter/twitter.py
+++ easy_post_twitter/twitter.py
@@ -202,21 +202,31 @@ class Twitter:
         except Exception as e:
             log.error(f'Error creating tweet. Error: {e}')
 
-    def tweetNthread(self, text):
+    def tweetNthread(self, text, isThread, id):
         ''' This method do not use twitter's api search to determine if
         a tweet is the day's opening one or a response to an early one. A state 
         local file keeps the state that informs the last day when the 
         state was last updated and the ID of the last tweet.
         '''
-        last_update = DataProcess.load_last_date(file=path + 'output/tsv/state.json')
-        today = date.today()
-        if today > last_update:
-          
-            client = self.__get_client()
+        #dir_path = Path(__file__).parent.parent.parent.parent.parent.parent
+        #file = dir_path / 'output/state.json'
+        #id, last_update = DataProcess.load_last_date(file=file)
+        #print(date.today(), '  ',last_update)
+        client = self.__get_client()
+        if isThread:
+            try:
+                print('Thread. Last tweet id:',id)
+                response = client.create_tweet(text=text, in_reply_to_tweet_id=id)
+                #DataProcess.update_state(file, response)
+                return response
+            except Exception as e:
+                log.error(f'Error creating thread. Error: {e}')
+        else:
             try:
                 response = client.create_tweet(text=text)
-                DataProcess.update_state(file=path + 'output/tsv/state.json',response=response)
+                #DataProcess.update_state(file, response)
                 log.info(f'Tweet created successfully. ID: {response.data["id"]}')
+                return response
             except Exception as e:
                 log.error(f'Error creating tweet. Error: {e}')
             
